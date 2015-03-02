62fc5662bb7f9aac5dd6cf13373b9aafc8b37d6b
benz <benz@example.com>
2015-03-01 23:44:02 -0500

    Finish static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,23 +3,38 @@ require 'rails_helper'
 describe "Static pages" do
 
 	describe "Home Page" do
-       it "should have the content 'Sample App'" do
+       it "should have the h1 'Sample App'" do
           visit '/static_pages/home'
-          expect(page).to have_content('Sample App')
+          expect(page).to have_selector('h1', :text => 'Sample App')
+       end
+       it "should have the title 'Home'" do
+    	  visit '/static_pages/home'
+    	  expect(page).to have_selector('title', 
+    	  	:text => "Ruby on Rails Sample App | Home")
        end
     end
 
     describe "Help page" do
-    	it "should have the content 'Help'" do
+    	it "should have the h1 'Help'" do
     		visit '/static_pages/help'
-    		expect(page).to have_content('Help')
+    		expect(page).to have_selector('h1', :text => 'Help')
     	end
+    	it "should have the title 'Help'" do
+    	  visit '/static_pages/help'
+    	  expect(page).to have_selector('title',
+                :text => "Ruby on Rails Sample App | Help")
+        end
     end
 
     describe "About page" do
-		it "should have the content 'About Us'" do
+		it "should have the h1 'About Us'" do
       		visit '/static_pages/about'
-      		expect(page).to have_content('About Us')
+      		expect(page).to have_selector('h1', :text => 'About Us')	
     	end
+    	it "should have the title 'About Us'" do
+    	  visit '/static_pages/about'
+    	  expect(page).to have_selector('title',
+                :text => "Ruby on Rails Sample App | About Us")
+        end
   	end
 end
